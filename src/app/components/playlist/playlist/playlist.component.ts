674e56f42463cc3856056ec0c758fc9934ebe4e1
Ale Fleitas <Alefleitas_94@hotmail.com>
2022-08-09 23:35:41 -0300

    button add new playlist + playlist table improvements were added

--- src/app/components/playlist/playlist/playlist.component.ts
+++ src/app/components/playlist/playlist/playlist.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Playlist } from '../../../models/playlist.model';
+import { PlaylistsService } from '../../../services/playlists.service';
+import { Observable } from 'rxjs';
 
+const ELEMENT_DATA: Playlist[] = [
+  { name: 'Pop Internacional', description: 'Enganchado de musica pop' },
+  { name: 'Rock Nacional Argentino', description: 'Enganchado de musica rock nacional argentino' },
+  { name: 'Reggaeton', description: 'Enganchado de musica reggaeton' },
+  { name: 'Cumbia', description: 'Enganchado de musica cumbia' }
+];
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.css']
 })
 export class PlaylistComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['name', 'description', 'action'];
+  // dataSource: Playlist[] = [];
+  dataSource: Playlist[] = ELEMENT_DATA;
+  constructor(
+    // private playlistService: PlaylistsService
+  ) { }
 
   ngOnInit(): void {
+
+    // this.playlistService.getAllPlaylists()
+    //   .subscribe({
+    //     next: (playlist) => {
+    //       this.dataSource = playlist;
+    //     },
+    //     error: (response) => {
+    //       console.log(response);
+
+    //     }
+    //   })
+
+
   }
 
 }
