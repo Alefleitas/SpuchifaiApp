71662e513833464c1b4bd383a60792b5c4a7c61c
Ale Fleitas <Alefleitas_94@hotmail.com>
2022-08-16 15:18:38 -0300

    login section + endpoint login + storage in localStorage for user authentication were added

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,14 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Auth } from '../../interfaces/auth.interface';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  password: string = '';
 
-  ngOnInit(): void {
+  user: Auth = {
+    usuario: '',
+    contraseña: ''
   }
 
+  constructor(
+    private router: Router,
+    private _authService: AuthService,
+    private snackBar: MatSnackBar,
+  ) { }
+
+  login() {
+    console.log(this._authService.postLogin(this.user));
+
+    this.user.contraseña = this.password;
+    this._authService.postLogin(this.user)
+      .subscribe(resp => {
+        console.log(resp);
+        this.router.navigate(['./playlist'])
+
+      }
+      )
+  }
+
+
+  showSnackbar(message: string) {
+    this.snackBar.open(message, 'ok!', {
+      duration: 5000
+    });
+
+  }
 }
