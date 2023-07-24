aebec8f225a0d642ad66accb09e12a407518cd4b
alejandro fleitas <afleitasinfo@gmail.com>
2023-07-24 13:56:08 -0300

    Update for tests

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent
-      }
+      },
+      {
+        path: 'register',
+        component: LoginComponent
+      },
       {
         path: '**',
         redirectTo: 'login'
